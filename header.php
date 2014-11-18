9c3e42fcbd38915a6b5de79f0d59526bba4af66e
nabila <nabila.habibah21@gmail.com>
2014-11-18 09:51:57 +0700

    versi1

---
+++ header.php
@@ -0,0 +1,21 @@
+
+
+<nav>  
+    <ul>
+        </li>
+		<li>Pembelian
+        <ul class="submenu"> 
+                <li><a href="#">Tambah Pemesanan</a></li>
+                <li><a href="#">Tambah Pembelian</a></li>
+                <li><a href="#">Retur Pembelian</a></li>
+        </ul>
+        </li>
+		<li>Kontak
+        <ul class="submenu"> 
+                <li><a href="#">Pemasok</a></li>
+                <li><a href="#">Penggguna</a></li>
+                <li><a href="#">Pelanggan</a></li>
+        </ul>
+        </li>
+    </ul> 
+</nav> 
