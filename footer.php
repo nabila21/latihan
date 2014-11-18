405c5494dc3326fdc3c28b321fd7a2f5634a189c
nabila <nabila.habibah21@gmail.com>
2014-11-18 09:31:42 +0700

    versi1

---
+++ footer.php
@@ -0,0 +1,15 @@
+<div id="footer"> 
+    <div id="column"> 
+        <h2></h2> 
+        Untuk mendapatkan petunjuk atau dukungan dari kami, kunjungi situs 
+        <a href="http://support.directnews.com">support.directnews.com</a> 
+        dan submit form anda. 
+    </div> 
+    <div id="column"> 
+        <h2>Hubungi Kami</h2> 
+        Jl. Parikesit 75<br> 
+        Surabaya 66666 Jawa Timur<br> 
+        (+6231) 5556667 
+    </div> 
+    
+</div>
